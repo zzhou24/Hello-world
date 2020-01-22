4b224df666e49ea91e61b54d38d78137d25f5917
zzhou24 <59131191+zzhou24@users.noreply.github.com>
2020-01-22 13:57:35 +0800

    Add file
    py file example

---
+++ multi_compare.py
@@ -0,0 +1,17 @@
+import multiprocessing as mp
+import time
+start_time=time.time()
+def job(x):
+    return x*x+x*x*x
+
+def multicore():
+    pool = mp.Pool(processes=90)
+    res = pool.map(job, range(50000))
+    return res
+if __name__ == '__main__':
+    final=[]
+    for i in range(50000):
+        res = job(i)
+        final.append(res)
+    print(final[-1])
+print(time.time()-start_time)
